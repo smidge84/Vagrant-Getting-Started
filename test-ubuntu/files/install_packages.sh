b42277b36c97bfd1e3872657180d6c3b617ccacc
Rich Robinson <richard.robinson@deltarail.com>
2024-09-10 18:35:25 +0100

    Provisioner to install packages

---
+++ test-ubuntu/files/install_packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Settings for unattended installs
+export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
+declare -r apt_opts='--assume-yes --option="Dpkg::Options::=--force-confdef" --option="Dpkg::Options::=--force-confold" --option="Dpkg::Options::=--force-unsafe-io"'
+
+echo ">> Updating APT cache"
+apt-get update
+
+echo ">> Upgrading all base packages"
+apt-get ${apt_opts} upgrade
+
+echo ">> Installing Bat package"
+apt-get ${apt_opts} install bat
